895119ffabb7d33d69f875b63329305ea936f96f
Carlos <carlos@xtrene.com>
2021-06-20 22:50:17 +0200

    Agregar archivos de proyecto.

---
+++ Tarea03_5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Tarea03_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 33;
+            int B = 20;
+            int C = 200;
+            int D = 57;
+            int temporal = 0;
+
+            Console.WriteLine("Valores iniciales de las variables:");
+            Console.WriteLine("Valor de A: {0}", A);
+            Console.WriteLine("Valor de B: {0}", B);
+            Console.WriteLine("Valor de C: {0}", C);
+            Console.WriteLine("Valor de D: {0}", D);
+
+            temporal = B;
+            B = C;
+            C = A;
+            A = D;
+            D = temporal;
+
+            Console.WriteLine("Valores de las variables después del cambio:");
+            Console.WriteLine("Valor de A: {0}", A);
+            Console.WriteLine("Valor de B: {0}", B);
+            Console.WriteLine("Valor de C: {0}", C);
+            Console.WriteLine("Valor de D: {0}", D);
+
+            Console.ReadKey();
+        }
+    }
+}
